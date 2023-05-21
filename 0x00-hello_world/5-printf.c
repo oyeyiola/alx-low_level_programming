56a6ae7cbbc7a72d2a8e08cdc329b607f52adac5
oyeyiola <oyeyiolarebecca08@gmail.com>
2023-05-21 11:24:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program with proper grammar
- * Return : 0 (success)
+ * main - Entry point
+ *
+ * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	      
+}
