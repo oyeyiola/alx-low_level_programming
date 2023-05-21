b86690e22e6a979e5b129f8ffec96c313f028b0d
oyeyiola <oyeyiolarebecca08@gmail.com>
2023-05-21 16:58:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: 0 (success)
+ * Description: main - prints out a sring to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
+puts("programming is like building a multillingual puzzle");
+return (0);
+}	       
