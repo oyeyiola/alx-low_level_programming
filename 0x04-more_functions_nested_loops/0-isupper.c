ebba5c26bb7dbd2f826238b2fe6bcaeadc01dafb
oyeyiola <oyeyiolarebecca08@gmail.com>
2023-05-27 06:17:52 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: 0 or 1
+* main - check the code
+*
+* Return: Always  0
 */
 
 int _isupper(int c)
