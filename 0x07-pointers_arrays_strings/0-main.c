269ad81f95f7a904440b566c87904f255a54e061
oyeyiola <oyeyiolarebecca08@gmail.com>
2023-06-05 18:54:05 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -11,33 +11,32 @@
 void simple_print_buffer(char *buffer, unsigned int size)
 {
         unsigned int i;
-
         i = 0;
         while (i < size)
         {
                 if (i % 10)
-                {                        printf(" ");
+                {
+                        printf(" ");
                 }
                 if (!(i % 10) && i)
                 {
                         printf("\n");
                 }
-                printf("0x%02x", buffer[i]);
-                i++;
+                printf("0x%02x", buffer[i]);                i++;
         }
         printf("\n");
 }
 
-/** * main - check the code
+/**
+ * main - check the code
  *
  * Return: Always 0.
- */
-int main(void)
+ */int main(void)
 {
     char buffer[98] = {0x00};
 
     simple_print_buffer(buffer, 98);
     _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");    simple_print_buffer(buffer, 98);    
-    return (0);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);        return (0);
 }
