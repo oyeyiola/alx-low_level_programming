803c0adeed1ca4d632ac808470fee762d7e090a4
oyeyiolarebecca <oyeyiolarebecca16@gmail.com>
2023-09-12 13:30:17 -0700

    main.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
-    char* name;
-    float age;
-    char* owner;
+#ifndef DOG_H
+#define DOG_H
+/*
+ * struct dog - a dog's basic info
+ * @name
+ * @age
+ * @owner
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
